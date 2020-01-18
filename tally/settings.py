930e6c2c41f532bbe63516ae047bc254d4ac3e51
Nov05 <arwen.liuwenjing@gmail.com>
2020-01-17 18:12:22 -0600

    2020-01-17 settings.py

--- tally/settings.py
+++ tally/settings.py
@@ -105,10 +105,6 @@ if 'RDS_HOSTNAME' in os.environ:
                 'ENGINE': 'django.db.backends.sqlite3',
             },
         },
-        'sqlite3': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
     }
 
 # Password validation
